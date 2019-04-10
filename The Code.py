60fe2d3632dbd1bab81e2d4923801593a6c76d89
Minecrafter18035 <49417847+Minecrafter18035@users.noreply.github.com>
2019-04-09 19:34:17 -0700

    Added championship winner

--- The Code.py
+++ The Code.py
@@ -1,4 +1,6 @@
 z = 0
+xx = 0
+yy = 0
 print ('x vs y ')
 print ('')
 while z != 3 :
@@ -8,9 +10,17 @@ while z != 3 :
   
   if x > y :
     print (str(z + 1) +'. ' 'X has defeated Y')
+    xx += 1
   elif x < y :
     print (str(z + 1) +'. ''Y has won')
+    yy += 1
   elif x == y :
     print (str(z + 1) +'. ''It is a tie this time')
-    
+
   z += 1
+if xx > yy :
+  print ('X has won the entire championship!')
+elif xx < yy :
+  print ('Y has won the entire championship!')
+elif xx == yy :
+  print ('No one has won :(')
