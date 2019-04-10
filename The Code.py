043b5ecc51be8e0c387d411ba4f6acb0b8ee9f7c
Minecrafter18035 <49417847+Minecrafter18035@users.noreply.github.com>
2019-04-09 19:10:53 -0700

    Made it run 3 different times

--- The Code.py
+++ The Code.py
@@ -1,12 +1,17 @@
-import random
-x = random.randint(1,10)
-y = random.randint(1,10)
-print ('x vs y ')
-
-print ('')
-if x > y :
-  print ('X has defeated Y')
-elif x < y :
-  print ('Y has won')
-elif x == y :
-  print ('It is a tie this time')
+z = 0
+while z != 3 :
+  print ('x vs y ')
+  print ('')
+  
+  import random
+  x = random.randint(0,10)
+  y = random.randint(0,10)
+  
+  if x > y :
+    print ('X has defeated Y')
+  elif x < y :
+    print ('Y has won')
+  elif x == y :
+    print ('It is a tie this time')
+    
+  z += 1
